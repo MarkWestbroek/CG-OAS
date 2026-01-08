53febddf8cea34a2d4d0c694db3811e45b632500
Mark Westbroek <github@msss.nl>
2026-01-08 13:06:47 +0100

    start
    init

---
+++ KVK/javascript/js genned/YAML gen EnumerationRepresentation.js
@@ -0,0 +1,74 @@
+function EnumerationRepresentation(manager, instanceName) {
+
+;
+this.m_Enumeration=null;
+;
+this.m_EA.Element=null;
+
+/// <summary>
+/// Deze functie maakt ook de Enumeration aan.
+/// </summary>
+/// <param name="enumRep"></param>
+this.EnumerationRepresentation = function(enumRep){
+
+};
+
+}//end EnumerationRepresentation
+
+
+
+function EnumerationRepresentations(manager, instanceName) {
+
+this.linkingAttributes=null;
+this.enumerationRepresentations=null;
+
+/// 
+/// <param name="linkingRep"></param>
+/// <param name="enumRep"></param>
+this.addEnum = function(linkingRep, enumRep){
+
+};
+
+/// 
+/// <param name="name"></param>
+this.getByName = function(name){
+
+};
+
+/// 
+/// <param name="guid"></param>
+this.getByLinkingAttributeGUID = function(guid){
+
+};
+
+/// 
+/// <param name="linkingRep"></param>
+/// <param name="enumRep"></param>
+this.findLinkingAttributes = function(linkingRep, enumRep){
+
+};
+
+}//end EnumerationRepresentations
+
+
+
+
+
+function LInkingAttributes(manager, instanceName) {
+
+;
+this.m_EA.Attribute=null;
+;
+this.m_EnumerationRepresentation=null;
+
+/// 
+/// <param name="attribute"></param>
+/// <param name="enumRep"></param>
+this.AddLinkingAttribute = function(attribute, enumRep){
+
+};
+
+}//end LInkingAttributes
+
+
+
